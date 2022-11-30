5ad11c0f4903b134aab3db5abac73c7aaf2cbb94
Danush Ramanan <dan.ramanan@gmail.com>
2022-11-30 07:12:16 -0700

    no going past edges

--- scripts/Player.js
+++ scripts/Player.js
@@ -1,13 +1,10 @@
 class Player {
-	constructor(x, y, width, height, speed, gravity, groundHeight) {
+	constructor(x, y, width, height, speed) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 		this.speed = speed;
-		this.gravity = gravity;
-		this.velocty = 0;
-		this.groundHeight;
 	}
 
 	move(dir) {
--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,20 @@
 const canvas = document.getElementById("canvas");
-if (!canvas.getContext) {
-	console.error("Cannot get 2D context of Canvas");
-	window.stop();
+try {
+	canvas.getContext("2d");
+} catch {
+	throw new Error("Cannot get 2D context of Canvas");
 }
+
 const ctx = canvas.getContext("2d");
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
 let running = true;
 
 const playerSprite = new Image();
 playerSprite.src = "sprites/player/mogus.jpg";
 
-let player = new Player(100, 100, 50, 100, 5, 10, canvas.height);
+// x, y, width, height, speed
+let player = new Player(canvas.width / 2, canvas.height / 2, 50, 100, 5);
 
 playerSprite.addEventListener("load", function() {
 	ctx.drawImage(playerSprite, player.x, player.y, player.width, player.height);
@@ -17,6 +22,11 @@ playerSprite.addEventListener("load", function() {
 
 window.addEventListener("keydown", function(event) {
 	player.move(event.key);
+	if (player.x <= 0) player.x = 0;
+	if (player.x + player.width >= canvas.width) player.x = canvas.width - player.width;
+	if (player.y <= 0) player.y = 0;
+	if (player.y + player.height >= canvas.height) player.y = canvas.height - player.height;
+	
 	
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.drawImage(playerSprite, player.x, player.y, player.width, player.height);
